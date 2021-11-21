f2df5b392d103f2ddf08a2c85b0916f19958c465
Brandon Sanders <brandonsanders5184@outlook.com>
2021-11-20 18:34:34 -0600

    Rearranged code to add extra commands and fixed a variable scope bug

--- NESdos/Program.cs
+++ NESdos/Program.cs
@@ -2,49 +2,58 @@
 
 while (true)
 {
-
+    //command prompt
     Console.Write("~");
     string searchTerm = Console.ReadLine();
-    if (searchTerm == "dir")
-    {
-        //gets the directory listing and puts it in the filePaths Array
-        string[] filePaths = Directory.GetFiles(@"C:\fceux\roms\");
-        //prints out every object in the filePaths array
-        Array.ForEach(filePaths, Console.WriteLine);
-    }
-    else
-    {
-        //allows user to enter filename of game if they so choose
-        string[] filePaths = Directory.GetFiles(@"C:\fceux\roms\", searchTerm + ".nes");
-        //prints out the file path
-        //puts the first file path into gameFile so it can be run
 
-        //if there is actually a file that is found run the file else do nothing
-        if (filePaths.Length > 0)
+    //allows user to enter filename of game if they so choose
+    string[] filePaths = Directory.GetFiles(@"C:\fceux\roms\", searchTerm + ".nes");
+
+    //if there is actually a file that is found run the file else run commands or say invalid syntax
+    if (filePaths.Length > 0)
+    {
+        //makes a new string that is the first file in the filePaths array so it can be run
+        string gameFile = filePaths.First().Trim();
+
+        //actually runs the fucking game
+        // Prepare the process to run
+        ProcessStartInfo start = new ProcessStartInfo();
+        // Enter in the command line arguments, everything you would enter after the executable name itself
+        start.Arguments = gameFile;
+        // Enter the executable to run, including the complete path
+        start.FileName = @"C:\fceux\fceux";
+        // Do you want to show a console window?
+        start.WindowStyle = ProcessWindowStyle.Hidden;
+        start.CreateNoWindow = true;
+        int exitCode;
+
+        // Run the external process & wait for it to finish
+        using (Process proc = Process.Start(start))
         {
-            string gameFile = filePaths.First().Trim();
+            proc.WaitForExit();
 
+            // Retrieve the app's exit code
+            exitCode = proc.ExitCode;
+        }
+    }
 
-            //actually runs the fucking game
-            // Prepare the process to run
-            ProcessStartInfo start = new ProcessStartInfo();
-            // Enter in the command line arguments, everything you would enter after the executable name itself
-            start.Arguments = gameFile;
-            // Enter the executable to run, including the complete path
-            start.FileName = @"C:\fceux\fceux";
-            // Do you want to show a console window?
-            start.WindowStyle = ProcessWindowStyle.Hidden;
-            start.CreateNoWindow = true;
-            int exitCode;
-
-            // Run the external process & wait for it to finish
-            using (Process proc = Process.Start(start))
-            {
-                proc.WaitForExit();
-
-                // Retrieve the app's exit code
-                exitCode = proc.ExitCode;
-            }
+    //where commands go
+    if (filePaths.Length == 0)
+    {
+        if (searchTerm == "dir")
+        {
+            //gets the directory listing and puts it in the filePaths Array
+            string[] dirListing = Directory.GetFiles(@"C:\fceux\roms\");
+            //prints out every object in the filePaths array
+            Array.ForEach(dirListing, Console.WriteLine);
+        }
+        if (searchTerm == "clear")
+        {
+            Console.Clear();
+        }
+        if (searchTerm == "exit")
+        {
+            Environment.Exit(0);
         }
         else
         {
@@ -52,4 +61,7 @@ while (true)
             continue;
         }
     }
+    
+    
+    
 }
